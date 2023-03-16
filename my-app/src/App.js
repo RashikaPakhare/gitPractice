d1fc1fbcc56f8f41b2344f9ffb6e17119db4b86d
Rashika <rashikapakhare99@gmail.com>
2023-03-16 18:36:46 +0530

    to check what happens after rebasing and making changes there only

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,8 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <div style= {{backgroundColor: "black"}}>
+     <div style= {{backgroundColor: "black", color: "white"}}>
       hiiiiiiiiii
+     </div>
+     <div>
+       <p>
+         these changes should not be or should be repleected in other repo?
+       </p>
      </div>
     </div>
   );
